fc3c67dd5471a378732c6b8052685466d01c195f
CharleeSF <charleetje@gmail.com>
2017-06-05 14:19:30 +0200

    Apparantly most of the time was spent in actually copying the values, then I realized that copying is completely uneccesary and I can just copy the pointers. doh! This resolves the whole speeddown issue (at least for 2d arrays but I"m expecting structs too, I'll try that now

--- cythoncode_redeclare.pyx
+++ cythoncode_redeclare.pyx
@@ -34,34 +34,28 @@ cdef extern from "stdint_compat.h":
     cdef int int_(double)
     cdef int int_(long double)
 
-cdef struct statevariable_container:
-    double* v
-    double* v0
-
-cdef fill_statevariable_arrays(_namespace, statevariable_container* statevariables, int N):
+cdef fill_statevariable_arrays(_namespace, double ***statevariables, int N):
     cdef _numpy.ndarray[double, ndim=1, mode='c'] _buf__array_neurongroup_v = _namespace['_array_neurongroup_v']    
     cdef double * _array_neurongroup_v = <double *> _buf__array_neurongroup_v.data
     cdef _numpy.ndarray[double, ndim=1, mode='c'] _buf__array_neurongroup_v0 = _namespace['_array_neurongroup_v0']
     cdef double * _array_neurongroup_v0 = <double *> _buf__array_neurongroup_v0.data
     cdef int _idx
 
-    statevariables.v = <double *>malloc(N*sizeof(double))
-    statevariables.v0 = <double *>malloc(N*sizeof(double))
-
-    for _idx in range(N):
-        statevariables.v[_idx] = _array_neurongroup_v[_idx]
-        statevariables.v0[_idx] = _array_neurongroup_v0[_idx]
+    statevariables[0] = <double **>malloc(2*sizeof(double *))
+    statevariables[0][0] = _array_neurongroup_v
+    statevariables[0][1] = _array_neurongroup_v0
 
     return 0
 
 
-cdef int unpack(int _idx, statevariable_container* statevariables) nogil:
-    cdef double v0
+cdef int unpack(int _idx, double ** statevariables) nogil:
     cdef double v
-    v0 = statevariables.v0[_idx]
-    v = statevariables.v[_idx]
-    statevariables.v0[_idx] = v0
-    statevariables.v[_idx] = v
+    cdef double v0
+    v = statevariables[0][_idx]
+    v0 = statevariables[1][_idx]
+    statevariables[0][_idx] = v
+    statevariables[1][_idx] = v0
+
     return 0
 
 def main(_namespace):
@@ -69,8 +63,8 @@ def main(_namespace):
     _var_N = _namespace["_var_N"]
     cdef int64_t N = _namespace["N"]
 
-    cdef statevariable_container *statevariables = <statevariable_container*>malloc(sizeof(statevariable_container))
-    fill_statevariable_arrays(_namespace, statevariables, N)
+    cdef double ** statevariables
+    fill_statevariable_arrays(_namespace, &statevariables, N)
 
     for _idx in range(N):    
         unpack(_idx, statevariables)
